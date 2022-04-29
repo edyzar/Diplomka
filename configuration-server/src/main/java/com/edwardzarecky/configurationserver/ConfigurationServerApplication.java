ae1cb0fafd69acabac1aecd4a3b06c60600802d5
edyzar <75585550+edyzar@users.noreply.github.com>
2022-04-29 18:05:13 +0200

    prvni

---
+++ configuration-server/src/main/java/com/edwardzarecky/configurationserver/ConfigurationServerApplication.java
@@ -0,0 +1,20 @@
+package com.edwardzarecky.configurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Edward Zářecký
+ * */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServerApplication.class, args);
+	}
+
+}
